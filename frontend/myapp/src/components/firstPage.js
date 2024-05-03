883c14242c6797fb874dd3e3bb3af66ea1bba1e0
Prayas Shrestha <73539544+PrayasXtha@users.noreply.github.com>
2024-05-03 10:51:34 -0400

    0.0.1

---
+++ frontend/myapp/src/components/firstPage.js
@@ -0,0 +1,51 @@
+// Navigation.js
+
+import React from "react";
+import Nav from "../components/navigation";
+
+function FirstPage() {
+  return (
+    <div>
+        
+
+        <div id="First_Page">
+        <div id="Nav">
+        
+
+            </div>
+            <div id="Right Content">
+            
+            <section class="text-gray-600 body-font overflow-hidden">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="lg:w-4/5 mx-auto flex flex-wrap">
+        
+    <div class="Navigation">
+       
+    <Nav/>
+      </div>
+
+
+      <div id="content_first">
+        <h2 class="text-sm title-font text-gray-500 tracking-widest">BRAND NAME</h2>
+        <h1 class="text-gray-900 text-3xl title-font font-medium mb-4">Animated Night Hill Illustrations</h1>
+
+        <p class="leading-relaxed mb-4">Fam locavore kickstarter distillery. Mixtape chillwave tumeric sriracha taximy chia microdosing tilde DIY. XOXO fam inxigo juiceramps cornhole raw denim forage brooklyn. Everyday carry +1 seitan poutine tumeric. Gastropub blue bottle austin listicle pour-over, neutra jean.</p>
+  
+        <img alt="ecommerce" class="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded" src="https://dummyimage.com/400x400"></img>
+   
+      </div>
+     
+    </div>
+  </div>
+</section>
+            </div>
+
+        </div>
+
+
+
+    </div>
+  );
+}
+
+export default FirstPage;
---
+++ frontend/myapp/src/components/navigation.js
@@ -0,0 +1,18 @@
+// Navigation.js
+
+import React from "react";
+
+function Navigation() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="#Project">Projects</a></li>
+        <li><a href="#contact">Start A Project</a></li>
+
+      </ul>
+    </nav>
+  );
+}
+
+export default Navigation;
